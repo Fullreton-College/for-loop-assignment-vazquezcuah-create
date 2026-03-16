55d19475177fc8736fdb616d7eded399a3e81dd8
Cuahutemoc Vazquez <vazquez.cuah@gmail.com>
2026-03-16 15:44:04 -0700

    Update Assignment2.cpp

--- Assignment2.cpp
+++ Assignment2.cpp
@@ -2,8 +2,34 @@
 using namespace std;
 
 int main() {
-
-   /* Type your code here. */
-
+int store1;
+int store2;
+int store3;
+int store4;
+int store5;
+   cout<< "Enter today's sales for store 1: " ;
+   cin>> store1;
+   cout<< endl << "Enter today's sales for store 2: " ;
+   cin>> store2;
+   cout<< endl << "Enter today's sales for store 3: ";
+   cin>> store3;
+   cout<< endl << "Enter today's sales for store 4: ";
+   cin>> store4;
+   cout<< endl<< "Enter today's sales for store 5: ";
+   cin>>store5;
+   int sales[]={store1 , store2 , store3 , store4 , store5};
+   int i=1;
+   while(i<=5){
+      cout<< endl << "Store "<< i <<":";
+      if(sales[i-1]>=100){
+      for(int star=100 ; star <= sales[i-1];star+=100){
+         cout<<"*";
+      }
+   }else{
+      cout<<"sales less than 100";
+   }
+    i++;
+   }
+   cout<<endl;
    return 0;
 }
