9fb811f8f93dd969dfd4ac581cad9ed92289b86e
Cuahutemoc Vazquez <vazquez.cuah@gmail.com>
2026-03-17 00:20:49 -0700

    Update Assignment3.1.cpp

--- Assignment3.1.cpp
+++ Assignment3.1.cpp
@@ -5,6 +5,21 @@ using namespace std;
 
 int main()
 {
+int num1;
+int num2;
+cout<< "Input any integer: " << endl;
+cin >> num1;
+cout << "Input a second integer (larger than the last): "<< endl;
+cin >> num2;
+if(num2 > num1){
+while( num1 < num2){
+    cout<<num1<<" ";
+    num1+=5;
+}
 
+}else{
+    cout<< "Second Integer can't be less than the first. Try Again.";
+}
+cout<<endl;
 return 0;
 }
